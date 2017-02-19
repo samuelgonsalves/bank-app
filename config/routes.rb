dd575f4a8b5be2ac0139d9419cb5a63b33a36175
Darshan Patel <dpatel12@ncsu.edu>
2017-02-19 12:40:07 -0800

    Admin features added

--- config/routes.rb
+++ config/routes.rb
@@ -10,20 +10,24 @@ Rails.application.routes.draw do
   resources :users
 
 
-  get '/admins/index' => 'admin#index', as: :admin_index
+  get '/admins/' => 'admin#index', as: :admin_index
   get '/admins/:id' => 'admin#home', as: :admin_home
   #get '/admins/:id/edit' => 'admin#edit'
-  get '/admins/:id/manage_users' => 'admin#manage_users', as: :manage_users
-  get '/admins/:id/manage_admins' => 'admin#manage_admins', as: :manage_admins
+  
   get '/admins/:id/manage_accounts' => 'admin#manage_accounts', as: :manage_accounts
+  get '/admins/:id/create_account' => 'admin#create_account', as: :create_account
+  get '/admins/:id/view_accounts' => 'admin#view_accounts', as: :view_accounts
+  get '/admins/:id/view_transaction_requests' => 'admin#view_transaction_requests', as: :view_transaction_requests
+  
+  get '/admins/:id/manage_users' => 'admin#manage_users', as: :manage_users
   get '/admins/:id/view_users' => 'admin#view_users', as: :view_users
-  post '/admins/:id/destroy_user' => 'admin#destroy_user', as: :destroy_user
   get '/admins/:id/view_transaction_history' => 'admin#view_transaction_history', as: :view_transaction_history
   get '/admins/:id/destroy_user' => 'admin#destroy_user', as: :destroy_user
-  #get '/admins/:id/create_admin' => 'admin#create_admin', as: :create_admin
-  #get '/admins/:id/view_admins' => 'admin#view_admins', as: :view_admins
+
+  get '/admins/:id/manage_admins' => 'admin#manage_admins', as: :manage_admins
+  get '/admins/:id/create_admin' => 'admin#create_admin', as: :create_admin
+  get '/admins/:id/view_admins' => 'admin#view_admins', as: :view_admins
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'static_pages#home'
 end
