5aa0a03e14831a7526e282a16d62c01fdf50b770
grathna <grathna@ncsu.edu>
2017-02-19 14:36:23 -0500

    Signup Bug Fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  
+  include UsersHelper
+
   def new
     logger.info("(#{self.class.to_s}) (#{action_name}) -- Entering the SignUp page")
   	@user = User.new
@@ -12,19 +13,27 @@ class UsersController < ApplicationController
   
   def create
     logger.info("(#{self.class.to_s}) (#{action_name}) -- Model action to create a new user in db")
-  	@user = User.new(user_params)
+    #Check if the user already exists in the DB if so redirect to error with message user already exists
+    puts params[:email]
+    @user = User.find_by_email(params[:email])
+    if !@user.nil?
+      #User already exists
+      flash[:success] = "User already exists!"
+      redirect_to login_url
+    end
+  	
+    @user = User.new(user_params)
   	if @user.save
-      log_in user
+      log_in @user
   		flash[:success] = "Welcome to the Sample App!"
-  		redirect_to welcome
+  		redirect_to login_url
   	else
   		render 'new'
   	end	
   end
 
   private
- 	 def user_params
-  		params.require(:user).permit(:name, :email,:password,:password_confirmation)
-  	 end
-
+ 	def user_params
+  	params.require(:user).permit(:name, :email,:password,:password_confirmation)
+  end
 end
