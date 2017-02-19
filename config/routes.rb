e106849aeee13594bb8e24a4d1711c9ccdfa7d35
Samuel <sgonsal@ncsu.edu>
2017-02-19 00:54:45 -0500

    resolving merge conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,5 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  get 'users/new'
-
-  get '/home' , to: 'static_pages#home'
-
-  get '/help' , to: 'static_pages#help'
-
-  get '/about' , to: 'static_pages#about'
-
-  get '/contact' , to: 'static_pages#contact'
-
-  get '/signup' , to: 'users#new'
-
-  get '/edit' , to: 'users#edit'
-
-  get 'users/:id/account_details' , to: 'users#account_details'
- 
-  patch '/edit' , to: 'users#update' #CHECK!
-
-  post '/signup' , to: 'users#create'
   
-=======
   root   'welcome#home'
   get    '/help',    to: 'static_pages#help'
   get    '/about',   to: 'static_pages#about'
@@ -29,6 +8,10 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
->>>>>>> dea1d33ec63df3811be41d87ff2aa96764cb9c5c
+  get '/edit' , to: 'users#edit'
+  get 'users/:id/account_details' , to: 'users#account_details'
+  patch '/edit' , to: 'users#update' #CHECK!
+
+
   resources :users
 end
