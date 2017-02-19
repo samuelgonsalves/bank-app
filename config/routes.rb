f59862ab0878c78a139ce97e145c9ad77893d517
Samuel <sgonsal@ncsu.edu>
2017-02-18 20:06:49 -0500

    Allows editing of user details

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,19 @@ Rails.application.routes.draw do
 
   get '/home' , to: 'static_pages#home'
 
-  get '/help' ,to: 'static_pages#help'
+  get '/help' , to: 'static_pages#help'
 
   get '/about' , to: 'static_pages#about'
 
-  get '/contact' ,to:'static_pages#contact'
+  get '/contact' , to: 'static_pages#contact'
 
-  get '/signup' ,to:'users#new'
+  get '/signup' , to: 'users#new'
 
-  post '/signup',  to: 'users#create'
+  post '/signup' , to: 'users#create'
+
+  get '/edit' , to: 'users#edit'
+
+  patch '/edit' , to: 'users#update' #CHECK!
 
   resources :users
 
